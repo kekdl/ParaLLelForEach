b7cbcae7c9e1a4e78bd72072261750336b043223
Sasha <sasha@sasha-N7x0WU>
2018-12-03 09:48:10 +0300

    ProcessorCount = 4

--- Program.cs
+++ Program.cs
@@ -13,12 +13,13 @@ namespace ParallelForEach
         static void Main()
    {            
       try {
-         TraverseTreeParallelForEach(@"/home/sasha/wargamming", (f) =>
+         TraverseTreeParallelForEach(@"/home/sasha/wargamming/1", (f) =>
          {
             // Exceptions are no-ops.
             try {
                // Do nothing with the data except read it.
-               byte[] data = File.ReadAllBytes(f);
+               // Ничего не делаю с данными, кроме чтения.
+               //byte[] data = File.ReadAllBytes(f);
             }
             catch (FileNotFoundException) {}
             catch (IOException) {}
@@ -39,14 +40,17 @@ namespace ParallelForEach
    public static void TraverseTreeParallelForEach(string root, Action<string> action)
    {
       //Count of files traversed and timer for diagnostic output
+      // Количество пройденных файлов и таймер для диагностического вывода
       int fileCount = 0;
       var sw = Stopwatch.StartNew();
 
       // Determine whether to parallelize file processing on each folder based on processor count.
-      int procCount = 15;
-      //System.Environment.ProcessorCount;
+      // Определите, следует ли распараллеливать обработку файлов в каждой папке на основе количества процессоров.
+      int procCount = 4;
+      Console.WriteLine("ProcessorCount = {0}; procCount = {1} ",System.Environment.ProcessorCount, procCount);
 
       // Data structure to hold names of subfolders to be examined for files.
+      // Структура данных для хранения имен подпапок для проверки файлов.
       Stack<string> dirs = new Stack<string>();
 
       if (!Directory.Exists(root)) {
@@ -63,11 +67,13 @@ namespace ParallelForEach
             subDirs = Directory.GetDirectories(currentDir);
          }
          // Thrown if we do not have discovery permission on the directory.
+         // Брошено, если у нас нет разрешения на открытие в каталоге.
          catch (UnauthorizedAccessException e) {
             Console.WriteLine(e.Message);
             continue;
          }
          // Thrown if another process has deleted the directory after we retrieved its name.
+         // если другой процесс удалил каталог после того, как мы получили его имя
          catch (DirectoryNotFoundException e) {
             Console.WriteLine(e.Message);
             continue;
@@ -92,6 +98,9 @@ namespace ParallelForEach
          // Execute in parallel if there are enough files in the directory.
          // Otherwise, execute sequentially.Files are opened and processed
          // synchronously but this could be modified to perform async I/O.
+         // Выполняем параллельно, если в каталоге достаточно файлов.
+         // В противном случае выполните последовательно. Файлы открываются и обрабатываются
+         // синхронно, но это может быть изменено для выполнения асинхронного ввода-вывода.
          try {
             if (files.Length < procCount) {
                foreach (var file in files) {
@@ -113,10 +122,12 @@ namespace ParallelForEach
             ae.Handle((ex) => {
                          if (ex is UnauthorizedAccessException) {
                             // Here we just output a message and go on.
+                            // Здесь мы просто выводим сообщение и продолжаем.
                             Console.WriteLine(ex.Message);
                             return true;
                          }
                          // Handle other exceptions here if necessary...
+                         // При необходимости обрабатывайте другие исключения ...
 
                          return false;
             });
@@ -124,11 +135,14 @@ namespace ParallelForEach
 
          // Push the subdirectories onto the stack for traversal.
          // This could also be done before handing the files.
+         // Вставьте подкаталоги в стек для обхода.
+         // Это можно сделать и перед передачей файлов.
          foreach (string str in subDirs)
             dirs.Push(str);
       }
 
       // For diagnostic purposes.
+      // Для диагностических целей.
       Console.WriteLine("Processed {0} files in {1} milliseconds", fileCount, sw.ElapsedMilliseconds);
    }
     }
